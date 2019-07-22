1280c787aef7dca510afa01c607fd59ab5e5e759
Mariusz Gumienny <temporary.gumienny@gmail.com>
2019-07-22 19:54:55 +0200

    add tabs

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  MatTabsModule
+} from '@angular/material';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTabsModule
+  ],
+  exports: [
+    MatTabsModule
   ]
 })
 export class MaterialModule { }
