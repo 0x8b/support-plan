28913bd8b3d31336a96f6e87d0bef7864e5ef05a
Mariusz Gumienny <temporary.gumienny@gmail.com>
2019-07-27 15:30:35 +0200

    add and update models

--- src/app/api.models.ts
+++ src/app/api.models.ts
@@ -1,15 +1,46 @@
-export interface Phone {
-  id:    number;
+export interface PhoneInterface {
+  id:    number | null;
   label: string;
   phone: string;
 }
 
-export interface SupportType {
-  id?:     number;
-  isUsed?: boolean;
+export interface SupportTypeInterface {
+  id:      number | null;
+  isUsed:  boolean | null;
   code:    string;
   charged: boolean;
   mature:  boolean;
-  order:   number;
-  phones:  Phone[];
+  order:   number | null;
+  phones:  PhoneInterface[];
+}
+
+export interface DateRangeInterface {
+  fromDate: string;
+  toDate:   string;
+}
+
+export interface DutyItemInterface {
+  dates:    DateRangeInterface[];
+  charged:  boolean;
+  mature:   boolean;
+  employee: DutyEmployeeInterface;
+  type:     string;
+  phone:    string | null;
+}
+
+export interface DutyEmployeeInterface {
+  login:    string;
+  fullname: string;
+  domain:   string;
+}
+
+export class SupportTypeCriteriaInterface {
+  code: string;
+  id:   number;
+}
+
+export interface DutyItemCriteria {
+  range: DateRangeInterface;
+  types: SupportTypeInterface[];
+  all:   boolean;
 }
